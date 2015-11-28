1b41bf1fab0ee781d0953f021dd91d439d2e4887
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-27 21:17:04 -0700

    s

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -3,9 +3,26 @@
 
 (defn -main [& args]
 
-(defn want [x] 
-  (or 
-    (= 0 (mod x 3)) 
+(defn want [x]
+  (or
+    (= 0 (mod x 3))
     (= 0 (mod x 5))))
 
-(println (apply + (filter want (range 1 1000)))))
+(println (str "Solves #1: " (apply +
+                                (filter want (range 1 1000)))))
+
+
+(defn fib [a b] (cons a
+                  (lazy-seq
+                     (fib b (+ a b)))))
+(defn want2 [n] (= 0.0 (mod n 2)))
+(defn lessThan [x] (< x 4000000))
+
+
+
+(println (str "Solves #2: " (apply +
+                              (filter want2
+                                (take-while lessThan (fib 1.0 1.0))))))
+
+
+)
